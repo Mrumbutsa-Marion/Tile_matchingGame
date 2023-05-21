0e30f6407d19a98c19e8f2b9d8726d97864602b2
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-05-21 19:46:19 +0300

    Adding the game container in javascript using the innerhtml which will allow it to link with html

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ let secondCard = false;
 
 //images arrays
 const items = [
-    {name: "cars", images: "cars.png"},
-    {name: "flush", images: "flush.png"},
-    {name: "game", images: "game.png"},
-    {name: "monitor", images: "monitor.png"},
-    {name: "plants", images: "plants.png"},
-    {name: "shower", images: "shower.png"},
-    {name: "utensils", images: "utensils.png"},
-    {name: "water_save", images: "water_save.png"},
+    {name: "cars", image: "cars.png"},
+    {name: "flush", image: "flush.png"},
+    {name: "game", image: "game.png"},
+    {name: "monitor", image: "monitor.png"},
+    {name: "plants", image: "plants.png"},
+    {name: "shower", image: "shower.png"},
+    {name: "utensils", image: "utensils.png"},
+    {name: "water_save", image: "water_save.png"},
 ];
 
 //The initial time 
@@ -44,3 +44,42 @@ const timeGenerator = () => {
     timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondValue}`;
 };
 
+
+//this is to show the calculating moves
+const movesCounter = () => {
+    moveCount += 1;
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
+};
+
+//This is for picking random objects from the images array
+const generateRandom = (size = 4) => {
+    let tempArray = [...items];
+    let cardValues = [];
+    size = (size * 3) / 2;
+
+    for (let i = 0; i < size; i++) {
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
+        cardValues.push(tempArray[randomIndex]);
+        tempArray.splice(randomIndex, 1);
+    }
+    return cardValues;
+};
+
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = "";
+    cardValues = [...cardValues, ...cardValues];
+
+    cardValues.sort(() => Math.random() -0.5);
+
+    for (let i = 0; i < size * 3; i++){
+        gameContainer.innerHTML += `
+        <div class="card-container" data-card-value="${cardValues[i].name}">
+        <div class="card-before"></div>
+        <div class="card-after">
+        <img src "${cardValues[i].image}" class="image"/></div>
+        </div>
+        `;
+    }
+
+    gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+}
