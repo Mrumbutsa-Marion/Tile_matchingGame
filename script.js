2ba3d29171a7c124d7699b8c02b8ca31746250a5
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-05-21 17:45:52 +0300

    styling the start and stop button

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ let cards;
 let interval; 
 let firstCard = false;
 let secondCard = false;
+
+
+//images arrays
+const items = [
+    {name: "cars", images: "cars.png"},
+    {name: "flush", images: "flush.png"},
+    {name: "game", images: "game.png"},
+    {name: "monitor", images: "monitor.png"},
+    {name: "plants", images: "plants.png"},
+    {name: "shower", images: "shower.png"},
+    {name: "utensils", images: "utensils.png"},
+    {name: "water_save", images: "water_save.png"},
+    
+]
